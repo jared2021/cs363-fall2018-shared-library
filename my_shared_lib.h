653d1eac7ea4ec2f67ed0009ab227a8f3ca49ed0
Jared Schoolcraft <jascho@tesla.cs.iupui.edu>
2018-12-07 14:10:59 -0500

    Pushing everything I have done so far.

---
+++ my_shared_lib.h
@@ -0,0 +1,15 @@
+#include <string>
+
+#include "my_shared_lib_export.h"
+
+MY_SHARED_LIB_Export
+void print_message (const std::string & msg);
+
+class MY_SHARED_LIB_Export
+{
+public:
+	Greeting(void);
+	~Greeting(void);
+
+	void say_message (const std::string & msg);
+}
